1fafe7d97d920b06c6292b3e2faa492a256de998
Old-Knitterhemd <fbnkrs@gmail.com>
2017-05-21 18:46:33 +0200

    allerlei
    matrizen zum tsten angelegt
    
    matAus Boolean für a[][] = null!?

--- src/Mat.java
+++ src/Mat.java
@@ -12,81 +12,88 @@ public class Mat
 	int crt = 1;
 
 	Mat()
-	{
-	}
+		{
+		}
 
 	Mat(BufferedReader br1) throws IOException
-	{
-		LinkedList<String> matList = new LinkedList<String>();
-		String pz;
-		while ((pz = br1.readLine()) != null)
-			{
-				matList.add(pz);
+		{
+			LinkedList<String> matList = new LinkedList<String>();
+			String pz;
+			while ((pz = br1.readLine()) != null)
+				{
+					matList.add(pz);
 
-			}
-		n = matList.size();
-		// Ersten Datensatz lesen und Spaltenanzahl m bestimmen
-		String[] sTemp = matList.get(0).split(";");
-		m = sTemp.length;
-		a = new double[n][m];
-		// weitere Datensätze einlesen und Spaltenanzahl überprüfen
-		for (int i = 0; i < n; i++)
-			{
-				sTemp = matList.get(i).split(";");
-				if (sTemp.length != m)
-					{
-						crt = -1;
-					} else
-					{
+				}
+			n = matList.size();
+			// Ersten Datensatz lesen und Spaltenanzahl m bestimmen
+			String[] sTemp = matList.get(0).split(";");
+			m = sTemp.length;
+			a = new double[n][m];
+			// weitere Datensätze einlesen und Spaltenanzahl überprüfen
+			for (int i = 0; i < n; i++)
+				{
+					sTemp = matList.get(i).split(";");
+					if (sTemp.length != m)
+						{
+							crt = -1;
+						} else
+						{
 
-						for (int j = 0; j < m; j++)
-							{
-								a[i][j] = Double.parseDouble(sTemp[j]);
-							}
-					}
+							for (int j = 0; j < m; j++)
+								{
+									a[i][j] = Double.parseDouble(sTemp[j]);
+								}
+						}
 
-			}
-	}
+				}
+		}
 
 	boolean matAus(FileWriter fx)
 		{
 			String csvTemp = "";
 			PrintWriter pr1 = new PrintWriter(fx);
-			for (int i = 0; i < n; i++)
+			if (a != null)
 				{
-
-					for (int j = 0; j < m; j++)
+					for (int i = 0; i < n; i++)
 						{
 
-							csvTemp += a[i][j];
-							if (j < (m - 1))
+							for (int j = 0; j < m; j++)
 								{
-									csvTemp += ";";
-								}
+									csvTemp += a[i][j]; // csv aus "a"
+														// generieren
 
+									if (j < (m - 1))
+										{
+											csvTemp += ";";
+										}
+
+								}
+							pr1.println(csvTemp);
+							csvTemp = "";
 						}
-					pr1.println(csvTemp);
-					csvTemp = "";
-				}
-			pr1.close();
-			return true;
+					pr1.close();
+					return true;
+				} else
+				return false;
 		}
 
 	Mat matMul(Mat b)
 		{
 			Mat resultat = new Mat();
-			
-			int m2=0, n2=0;
-			
+
+			int m2 = 0, n2 = 0;
+
 			if (b.a.length == a[0].length)
 				{ // Prüfung ob Matrizen multiplizierbar
 					n2 = a.length;
 					m2 = b.a[0].length;
-					resultat.a = new double[n2][m2]; //Hilfsarray entprechend der Zeilen und Spalten erstellen
+					resultat.a = new double[n2][m2]; // Hilfsarray entprechend
+														// der Zeilen und
+														// Spalten erstellen
 
-					for (int i = 0; i < n2; i++) //Zeilen
+					for (int i = 0; i < n2; i++) // Zeilen
 						{
-							for (int j = 0; j < m2; j++) //Spalten
+							for (int j = 0; j < m2; j++) // Spalten
 								{
 
 									for (int k = 0; k < b.a.length; k++) // Multiplikation
@@ -95,7 +102,7 @@ public class Mat
 										}
 								}
 						}
-					
+
 				} else
 				{
 					resultat.crt = -3;
--- src/MatAnw.java
+++ src/MatAnw.java
@@ -2,39 +2,170 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MatAnw
 {
 
 	public static void main(String[] args) throws IOException
 		{
+			// File/Buffered -reader, Filewriter
+			// FileReader fr1 = new FileReader("mat_.txt");
+			FileWriter fw1 = new FileWriter("test_.txt");
+			// BufferedReader br1 = new BufferedReader(fr1);
 
-			FileWriter fw1 = new FileWriter("test1_Outfile.txt");
-			FileWriter fw2 = new FileWriter("test2_Outfile.txt");
-			FileWriter fwR = new FileWriter("Ergebnis_MatMul.txt");
-			FileReader fr1 = new FileReader("Mat1.txt");
-			FileReader fr2 = new FileReader("Mat2.txt");
-
-			BufferedReader br1 = new BufferedReader(fr1);
+			FileReader fr2 = new FileReader("mat3_3.txt");
+			FileWriter fw2 = new FileWriter("test3_3.txt");
 			BufferedReader br2 = new BufferedReader(fr2);
 
-			Mat testMat1 = new Mat(br1);
-			Mat testMat2 = new Mat(br2);
-			Mat resultatMatMul = new Mat();
+			FileReader fr3 = new FileReader("mat3_2.txt");
+			FileWriter fw3 = new FileWriter("test3_2.txt");
+			BufferedReader br3 = new BufferedReader(fr3);
 
-			// Ausgaben und Multiplikation
+			FileReader fr4 = new FileReader("mat5_6.txt");
+			FileWriter fw4 = new FileWriter("test5_6.txt");
+			BufferedReader br4 = new BufferedReader(fr4);
 
-			if (testMat1.matMul(testMat2).crt != -3)
+			FileReader fr5 = new FileReader("mat6_7.txt");
+			FileWriter fw5 = new FileWriter("test6_7.txt");
+			BufferedReader br5 = new BufferedReader(fr5);
+
+			// Matrizen
+			Mat mat_ = new Mat();
+			if (mat_.matAus(fw1))
 				{
-
-					resultatMatMul = testMat1.matMul(testMat2);
-					resultatMatMul.matAus(fwR);
-
+					System.out.println("GUT");
 				} else
-				System.out.println("Multiplizieren nicht möglich");
-			testMat1.matAus(fw1);
-			
-			testMat2.matAus(fw2);
-		}
+				System.out.println("Schlecht");
 
+			Mat mat3_3 = new Mat(br2);
+			if (mat3_3.matAus(fw2))
+				{
+					System.out.println("GUT");
+				} else
+				System.out.println("Schlecht");
+			Mat mat3_2 = new Mat(br3);
+			if (mat3_2.matAus(fw3))
+				{
+					System.out.println("GUT");
+				} else
+				System.out.println("Schlecht");
+			Mat mat5_6 = new Mat(br4);
+			if (mat5_6.matAus(fw4))
+				{
+					System.out.println("GUT");
+				} else
+				System.out.println("Schlecht");
+			Mat mat6_7 = new Mat(br5);
+			if (mat6_7.matAus(fw5))
+				{
+					System.out.println("GUT");
+				} else
+				System.out.println("Schlecht");
+
+			// Multiplikation Ergebnis Matrix
+			FileWriter fwR = new FileWriter("Ergebnis_MatMul.txt");
+			Mat resMatMul = new Mat();
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			int choice = 0;
+
+			do
+				{
+					// Main Menü
+					System.out.println();
+					System.out.println("Bitte waehlen:");
+					System.out.println("(1) mat_ * mat3_3");
+					System.out.println("(2) mat3_3 * mat3_2");
+					System.out.println("(3) mat5_6 * mat_6_7");
+					System.out.println("(4) mat5_6 * mat3_3");
+					System.out.println("(5) mat3_3 * mat3_3");
+
+					System.out.println("(0) Ende");
+
+					choice = Integer.parseInt(in.readLine());
+					System.out.println();
+
+					switch (choice)
+						{
+
+						case 1:
+							if (mat_.matMul(mat3_3).crt != -3)
+								{
+
+									resMatMul = mat_.matMul(mat3_3);
+									if (resMatMul.matAus(fwR))
+										{
+											System.out.println("GUT");
+										} else
+										System.out.println("Schlecht");
+
+								} else
+								System.out.println("Multiplizieren nicht möglich");
+
+							break;
+
+						case 2:
+							if (mat3_3.matMul(mat3_2).crt != -3)
+								{
+
+									resMatMul = mat3_3.matMul(mat3_2);
+									if (resMatMul.matAus(fwR))
+										{
+											System.out.println("GUT");
+										} else
+										System.out.println("Schlecht");
+
+								} else
+								System.out.println("Multiplizieren nicht möglich");
+							break;
+
+						case 3:
+							if (mat5_6.matMul(mat6_7).crt != -3)
+								{
+
+									resMatMul = mat5_6.matMul(mat6_7);
+									if (resMatMul.matAus(fwR))
+										{
+											System.out.println("GUT");
+										} else
+										System.out.println("Schlecht");
+
+								} else
+								System.out.println("Multiplizieren nicht möglich");
+							break;
+
+						case 4:
+							if (mat5_6.matMul(mat3_3).crt != -3)
+								{
+
+									resMatMul = mat5_6.matMul(mat3_3);
+									if (resMatMul.matAus(fwR))
+										{
+											System.out.println("GUT");
+										} else
+										System.out.println("Schlecht");
+
+								} else
+								System.out.println("Multiplizieren nicht möglich");
+							break;
+
+						case 5:
+							if (mat3_3.matMul(mat3_3).crt != -3)
+								{
+
+									resMatMul = mat3_3.matMul(mat3_3);
+									if (resMatMul.matAus(fwR))
+										{
+											System.out.println("GUT");
+										} else
+										System.out.println("Schlecht");
+
+								} else
+								System.out.println("Multiplizieren nicht möglich");
+							break;
+						}
+				} while (choice != 0);
+
+		}
 }
