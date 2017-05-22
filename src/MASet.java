b7132957debfd37e8b3a11c9ac928ed43aed274e
Old-Knitterhemd <fbnkrs@gmail.com>
2017-05-22 20:19:54 +0200

    MASet(); verändert
    Boolean [] und Ausgaben für , ob korrekt eingetragen.

--- src/MASet.java
+++ src/MASet.java
@@ -1,19 +1,19 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
+
 import java.util.TreeSet;
 
 public class MASet
 {
 	TreeSet<MaschineS> tmenge = new TreeSet<MaschineS>();
 
-	MASet(){}
-	
+	MASet()
+	{
+	}
+
 	int dat2tset(BufferedReader br1) throws IOException
 		{
-			
+
 			int anzahl = 0;
 			String input;
 
@@ -21,14 +21,19 @@ public class MASet
 				{
 					if ((new MaschineS(input).getCrt()) == 1)
 						{
-						boolean wasSet;
-						wasSet= tmenge.add(new MaschineS(input));
-							anzahl++;
-							
+							if (tmenge.add(new MaschineS(input)))
+								anzahl++;
 						}
 				}
-
-			ein.close();
+			boolean[] wasAdded = new boolean[anzahl];
+			for (int i = 0; i < anzahl; i++)
+				{
+					if (wasAdded[i])
+						System.out.println((i + 1) + "te Maschine wurde eingetragen!\n");
+					else
+						System.out.println((i + 1) + "te Maschine wurde NICHT eingetragen!\n");
+				}
+			br1.close();
 			return anzahl;
 		}
 
